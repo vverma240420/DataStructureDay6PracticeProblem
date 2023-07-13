053232f829574aa9211e24449362db4740c82783
Vivek Verma <vverma240420@gmail.com>
2023-07-13 20:48:34 +0530

    Welcome to data structure program

---
+++ DataStructureDay6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataStructureDay6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Data Structure Programs");
+        }
+    }
+}
